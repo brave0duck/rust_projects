f1e9b9cff52df0f2bc5392cd333d2f5889007f70
brave0duck <brave0duck@gmail.com>
2026-06-14 23:46:20 +0900

    ==

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,25 @@
 fn main() {
-    let var1 : u32 = 100;
+    let var0 : Option<i32> = Option::None;
+    let var1  : Option<&str> = Option::Some("Hello, world!");
+    let var2  : Option<i32> = Option::Some(23);
 
-    println!("Hello, world!");
-    println!("var1 : {}", var1);
+    println!("before fn - var0: {:?}, var1 : {:?}, var2 : {:?}", var0 ,var1 ,var2);
+
+    let var0 = check_data(&var0);
+    let var1 = check_data(&var1);
+    let var2 = check_data(&var2);
+
+    println!("before fn - var0: {:?}, var1 : {:?}, var2 : {:?}", var0 ,var1 ,var2);
+}
+fn check_data<T: std::fmt::Debug + Copy>(s : &Option<T>) -> Option<T> {
+    match s{
+        Some(i) => {
+            println!("Here is in func. Some - {:?}", i);
+            Some(*i)
+        },
+        None => {
+            println!("Here is in func. data - None");
+            None
+        }
+    }
 }
---
+++ hello_world/target/debug/deps/hello_world-e860db010f2bedf6.d
@@ -0,0 +1,5 @@
+/home/speedking/workspace/rust_projects/hello_world/target/debug/deps/hello_world-e860db010f2bedf6.d: src/main.rs
+
+/home/speedking/workspace/rust_projects/hello_world/target/debug/deps/libhello_world-e860db010f2bedf6.rmeta: src/main.rs
+
+src/main.rs:
