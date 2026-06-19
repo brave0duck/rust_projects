25f6aa1ab30d79b4b0b2546cc6af8ebb3d0654e4
brave0duck <brave0duck@gmail.com>
2026-06-19 18:36:03 +0900

    -ing

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -23,23 +23,10 @@ struct Humidifier{
     h_step : HumSpeed,
 }
 impl Humidifier{
-    fn new(mode : u32, step : u32) -> Humidifier{
+    fn new() -> Humidifier{
         Humidifier{
-            h_mode : match mode{
-                1 => HumMode::Auto,
-                2 => HumMode::Target,
-                3 => HumMode::Minimum,
-                _ => HumMode::Auto,
-            },
-            h_step : match step{
-                0 => HumSpeed::Zero,
-                1 => HumSpeed::One,
-                2 => HumSpeed::Two,
-                3 => HumSpeed::Three,
-                4 => HumSpeed::Four,
-                5 => HumSpeed::Five,
-                _ => HumSpeed::Zero,
-            }
+            h_mode : HumMode::Auto,
+            h_step : HumSpeed::Zero,
         }
     }
     fn setup(&mut self, mode : u32, step : u32){
@@ -65,7 +52,7 @@ fn main() {
 
     let mut input_mode  = String::new();
     let mut input_step = String::new();
-    let mut hum = Humidifier::new(1,1);
+    let mut hum = Humidifier::new();
     
 
     println!("가습기 모드와 출력을 선택하세요");
@@ -80,9 +67,9 @@ fn main() {
     let input_step : u32 = input_step.trim().parse().expect("failed to convert");
 
     hum.setup(input_mode, input_step);
-    state_humidifier(&hum);
+    print_state(&hum);
 
 }
-fn state_humidifier(h : & Humidifier) {
-    println!("==> 현재 가습기모드는 [{:?}]이고 출력단계는 [{:?}]단계입니다.", h.h_mode, h.h_step);
+fn print_state(h : & Humidifier) {
+    println!("==> 현재 가습기모드는 [{:?} 출력단계는 [{:?}]단계입니다.", h.h_mode, h.h_step);
 }
--- exercise/target/debug/exercise.d
+++
@@ -1,1 +0,0 @@
-/home/speedking/workspace/rust_projects/exercise/target/debug/exercise: /home/speedking/workspace/rust_projects/exercise/src/main.rs
